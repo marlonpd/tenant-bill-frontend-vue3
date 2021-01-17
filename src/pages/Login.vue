6633da535f149a132f36801ab3adcf5927ecc278
marlonpd <send8heremd@gmail.com>
2021-01-17 12:52:17 +0800

    Downgrade autofixer

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,47 +1,6 @@
 <template>
   <div class="container">
-    <div id="formContent" class="mx-auto">
-      <!-- Tabs Titles -->
-
-      <!-- Icon -->
-      <div class="fadeIn first m-6">
-        <h4>Welcome</h4>
-        <ul v-if="errors" class="error-messages">
-          <li v-for="(v, k) in errors" :key="k">{{ v | error }}</li>
-        </ul>
-      </div>
-
-      <!-- Login Form -->
-      <form @submit.prevent="onSubmit">
-        <input
-          type="text"
-          id="email"
-          class="fadeIn second"
-          v-model="email"
-          name="email"
-          placeholder="email"
-        />
-        <input
-          type="password"
-          id="password"
-          class="fadeIn third"
-          v-model="password"
-          name="password"
-          placeholder="password"
-        />
-        <input type="submit" class="fadeIn fourth" value="Log In" />
-      </form>
-
-      <!-- Remind Passowrd -->
-      <div id="formFooter">
-        <p class="text-xs-center">
-          <router-link :to="{ name: 'register' }">
-            Need an account?
-          </router-link>
-        </p>
-        <a class="underlineHover" href="#">Forgot Password?</a>
-      </div>
-    </div>
+    <div id="formContent" class="mx-auto"></div>
   </div>
 </template>
 
@@ -49,7 +8,7 @@
   import { defineComponent } from 'vue';
 
   export default defineComponent({
-    name: 'HomePage',
+    name: 'LoginPage',
     components: {},
   });
 </script>
